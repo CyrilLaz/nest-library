a34d24c5c77dba783b43a805cac7186334bd7c40
Cyril_Lazar <klazar@mail.ru>
2024-06-28 01:04:08 +0700

    feat: config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { BooksModule } from "./books/books.module";
 import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [BooksModule, MongooseModule.forRoot('mongodb://localhost:27017/lib')],
+  imports: [
+    ConfigModule.forRoot(),
+    BooksModule,
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
